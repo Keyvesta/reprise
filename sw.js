549477988a82551cdb725f58c2299ae0a58a8b13
Keyvesta <marcemirian@hotmail.com>
2026-08-27 17:05:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "reprise-v3-4";
+const CACHE = "reprise-v3-5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
